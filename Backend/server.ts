81bb24617f3ee2df6860a409e659ad4e2244c8a4
vivek kumar verma <vivek23vermasrivastava@gmail.com>
2026-05-22 22:24:46 +0530

    fix render timeout

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,7 +1,7 @@
 import app from './src/app.js'
 
-app.listen(3000,()=>
-{
-    console.log("server is running on the port 3000");
-    
+
+
+app.listen(3000, "0.0.0.0", () => {
+    console.log(`server is running on the port 3000`);
 })
